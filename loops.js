78a4910c8374680c93737ae7c0a6acf2a9eef282
Joshua Wessels <joshuawessels04@gmail.com>
2020-03-04 17:33:45 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,10 +1,14 @@
  function forLoop(array) {
   for (let i = 0; i < 25; i++) {
   array.push(`I am ${i} strange loop${i === 1 ? '' : 's'}.`);
-   }
+  }
     return array;
  }
 
-function whileLoop(n) {
-  
+function whileLoop (n) {
+  while (n > 0) {
+   console.log( --n);
+  }
+  return "done";
 }
+
